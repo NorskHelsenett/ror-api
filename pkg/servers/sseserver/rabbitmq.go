f4413c9a5fddf110d8f9089a1a30d893bb28b991
Håvard Elnan <havard.elnan@nhn.no>
2025-09-23 19:42:53 +0200

    Refactor RabbitMQ configuration: remove unused exchange declarations and bind logic; enhance listener config with exchange properties.

--- pkg/servers/sseserver/rabbitmq.go
+++ pkg/servers/sseserver/rabbitmq.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/NorskHelsenett/ror-api/internal/apiconnections"
 	"github.com/NorskHelsenett/ror/pkg/handlers/rabbitmqhandler"
-	"github.com/NorskHelsenett/ror/pkg/messagebuscontracts"
 	"github.com/NorskHelsenett/ror/pkg/rlog"
 	"github.com/google/uuid"
 	"github.com/rabbitmq/amqp091-go"
@@ -22,67 +21,24 @@ const (
 
 func StartListeningRabbitMQ() {
 
-	err := apiconnections.RabbitMQConnection.GetChannel().ExchangeDeclare(
-		SSEventsExchange, // name
-		"fanout",         // kind
-		true,             // durable
-		true,             // autoDelete -> delete when unused
-		false,            // internal
-		false,            // no-wait
-		nil,              // arguments
-	)
-	if err != nil {
-		rlog.Fatal("Could not declare excahnge", err)
-	}
-
-	err = apiconnections.RabbitMQConnection.GetChannel().ExchangeBind(
-		SSEventsExchange,                //destination
-		"eventv2.#",                     // key
-		messagebuscontracts.ExchangeRor, // source
-		false,                           // noWait
-		nil,                             // arguments
-	)
-	if err != nil {
-		panic(err)
-	}
-
 	// //Create the queue
 	SSEventsQueueName := fmt.Sprintf("%s-%s", SSEventsQueueNamePrefix, uuid.New().String())
-	// apiEventsqueue, err := apiconnections.RabbitMQConnection.GetChannel().QueueDeclare(
-	// 	SSEventsQueueName, // name
-	// 	false,             // durable
-	// 	true,              // delete when unused
-	// 	false,             // exclusive
-	// 	false,             // no-wait
-	// 	nil,               // arguments, non quorum queue
-	// )
-	// if err != nil {
-	// 	rlog.Fatal("Could not declare queue", err)
-	// }
-
-	// err = apiconnections.RabbitMQConnection.GetChannel().QueueBind(
-	// 	apiEventsqueue.Name, // queue name
-	// 	"",                  // routing key
-	// 	SSEventsExchange,    // exchange
-	// 	false,
-	// 	nil,
-	// )
-	// if err != nil {
-	// 	rlog.Fatal("Could not bind queue to excahnge", err)
-	// }
 
 	go func() {
 		config := rabbitmqhandler.RabbitMQListnerConfig{
-			Client:          apiconnections.RabbitMQConnection,
-			QueueName:       SSEventsQueueName,
-			Consumer:        "",
-			AutoAck:         false,
-			QueueAutoDelete: true,
-			Exclusive:       false,
-			NoLocal:         false,
-			NoWait:          false,
-			Args:            nil,
-			Exchange:        SSEventsExchange,
+			Client:             apiconnections.RabbitMQConnection,
+			QueueName:          SSEventsQueueName,
+			Consumer:           "",
+			AutoAck:            false,
+			QueueAutoDelete:    true,
+			Exclusive:          false,
+			NoLocal:            false,
+			NoWait:             false,
+			Args:               nil,
+			Exchange:           SSEventsExchange,
+			ExcahngeKind:       "fanout",
+			ExchangeAutoDelete: true,
+			ExcahngeDurable:    true,
 		}
 		rabbithandler := rabbitmqhandler.New(config, ssemessagehandler{})
 		_ = apiconnections.RabbitMQConnection.RegisterHandler(rabbithandler)
