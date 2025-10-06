c1d109ea3e263dc85daef3fe79f40fa16618d570
Håvard Elnan <havard.elnan@nhn.no>
2025-10-06 10:47:17 +0200

    Update health check implementation to use go-health package and clean up unused code

--- internal/controllers/healthcontroller/health_controller.go
+++ internal/controllers/healthcontroller/health_controller.go
@@ -12,18 +12,16 @@ import (
 	"github.com/NorskHelsenett/ror-api/internal/apiconnections"
 	"github.com/NorskHelsenett/ror-api/internal/responses"
 
-	"github.com/NorskHelsenett/ror/pkg/config/configconsts"
-
-	"github.com/NorskHelsenett/ror/pkg/apicontracts"
+	"github.com/NorskHelsenett/ror/pkg/helpers/rorerror"
 
 	"github.com/NorskHelsenett/ror/pkg/clients/redisdb"
 
 	"github.com/NorskHelsenett/ror/pkg/clients/mongodb"
 
 	"github.com/NorskHelsenett/ror/pkg/rlog"
+	"github.com/dotse/go-health"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 	"go.opentelemetry.io/otel"
 )
 
@@ -106,33 +104,19 @@ func GetTracingStatus(ctx context.Context) responses.HealthStatusCode {
 //	@Tags			health status
 //	@Accept			application/json
 //	@Produce		application/json
-//	@Success		200	{object}	apicontracts.HealthStatus
-//	@Failure		500	{string}	Failure	message
+//	@Success		200	{object}	health.Response
+//	@Failure		500	{object}	Failure	message
 //	@Router			/health [get]
 func GetHealthStatus() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var healthstatus responses.HealthStatus
-		var services []responses.Services
-		httpStatus := http.StatusOK
-		services = append(services, responses.Services{Name: "identityProvider", Status: PingAndUpdateStatus(viper.GetString(configconsts.OIDC_PROVIDER), c.Request.Context())})
-		services = append(services, responses.Services{Name: "database", Status: GetMongoDBStatus(c.Request.Context())})
-		services = append(services, responses.Services{Name: "messageBus", Status: GetRabbitMQStatus(c.Request.Context())})
-		services = append(services, responses.Services{Name: "secrets", Status: PingAndUpdateStatus(viper.GetString(configconsts.VAULT_URL), c.Request.Context())})
-		//services = append(services, responses.Services{Name: "redis", Status: GetRedisStatus(c.Request.Context())})
-
-		// services = append(services, responses.Services{Name: "tracing", Status: GetTracingStatus(c.Request.Context())})
-		healthstatus.Services = services
-
-		for _, v := range services {
-			if v.Status != responses.StatusOK && v.Name != "messageBus" && v.Name != "tracing" {
-				httpStatus = http.StatusInternalServerError
-			}
+		var healthstatus *health.Response
+		var err error
+		healthstatus, err = health.CheckHealth(context.Background())
+		if err != nil {
+			rerr := rorerror.NewRorError(500, "could not get health status")
+			rerr.GinLogErrorAbort(c)
+			return
 		}
-
-		// importing apicontracts for swagger
-		var _ apicontracts.HealthStatus
-		c.JSON(httpStatus,
-			healthstatus,
-		)
+		c.JSON(http.StatusOK, healthstatus)
 	}
 }
