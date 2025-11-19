a6ea6c7f7cf4700f090b69cd5f639f4ea8df7faf
Håvard Elnan <havard.elnan@nhn.no>
2025-11-19 11:45:51 +0100

    feat: refactor token management by integrating token storage helper and removing legacy key storage implementation

--- pkg/services/tokenservice/tokenservice.go
+++ pkg/services/tokenservice/tokenservice.go
@@ -2,17 +2,13 @@ package tokenservice
 
 import (
 	"context"
-	"crypto"
-	"crypto/rand"
-	"crypto/rsa"
 	"errors"
 	"fmt"
-	"math/big"
 	"strings"
 	"time"
 
-	"github.com/NorskHelsenett/ror-api/internal/apiconnections"
 	"github.com/NorskHelsenett/ror/pkg/helpers/fouramhelper"
+	"github.com/NorskHelsenett/ror/pkg/helpers/tokenstoragehelper"
 	identitymodels "github.com/NorskHelsenett/ror/pkg/models/identity"
 	"github.com/NorskHelsenett/ror/pkg/rlog"
 	"github.com/coreos/go-oidc/v3/oidc"
@@ -25,71 +21,10 @@ import (
 // 2. Implement key rotation OK
 // 3. Implement support for multiple oidc providers and client ids with check on domain name
 
-const (
-	VAULT_PATH = "secret/data/v1.0/ror/config/token"
-)
-
 var (
 	oidcProviderURL string = "https://auth.sky.nhn.no/dex"
 	oidcClientId    string = "clusterauth"
-	keyStorage      KeyStorage
 )
-
-func RotateKeys() {
-	if keyStorage.needRotate(false) {
-
-		randomInterval, err := rand.Int(rand.Reader, big.NewInt(5000))
-		if err != nil {
-			rlog.Error("could not generate random interval for key rotation", err)
-			return
-		}
-		time.Sleep(time.Duration(time.Duration(randomInterval.Int64()) * time.Millisecond))
-		err = keyStorage.Load()
-		if err != nil {
-			rlog.Error("could not load keystorage from vault", err)
-			return
-		}
-		rotated := keyStorage.rotate(true)
-		if rotated {
-			err := keyStorage.Save()
-			if err != nil {
-				rlog.Error("could not save keystorage to vault", err)
-			}
-		}
-		rlog.Info("Key rotation completed")
-	}
-}
-
-func GenerateKey() (Key, error) {
-	newPrivatekey, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return Key{}, err
-	}
-
-	thumprint, err := jwk.FromRaw(newPrivatekey.PublicKey)
-	if err != nil {
-		return Key{}, err
-	}
-	keyid, err := thumprint.Thumbprint(crypto.SHA256)
-	if err != nil {
-		return Key{}, err
-	}
-	key := Key{
-		KeyID:        fmt.Sprintf("%x", keyid),
-		PrivateKey:   newPrivatekey,
-		AlgorithmKey: "RS512",
-	}
-	return key, nil
-}
-
-func Init() {
-	keyStorage.storageProvider = NewVaultStorageAdapter(apiconnections.VaultClient, VAULT_PATH)
-	err := keyStorage.Load()
-	if err != nil {
-		rlog.Error("could not load keystorage from vault", err)
-	}
-	RotateKeys()
-}
 
 // ExchangeToken exchanges a token for a new resigned token
 // 1 . Verifies the provided token
@@ -131,7 +66,7 @@ func ExchangeToken(ctx context.Context, clusterID string, token string, admin bo
 
 	exp := fouramhelper.FourAm()
 
-	newtoken := jwt.NewWithClaims(jwt.GetSigningMethod(keyStorage.GetCurrentKey().AlgorithmKey), jwt.MapClaims{
+	claims := jwt.MapClaims{
 		"sub":              user.Email,
 		"iss":              "https://auth.ror.nhn.no",
 		"email":            user.Email,
@@ -143,9 +78,11 @@ func ExchangeToken(ctx context.Context, clusterID string, token string, admin bo
 		"clusterID":        clusterID,
 		"providerISS":      user.Issuer,
 		"providerAudience": user.Audience,
-	})
-	newtoken.Header["kid"] = keyStorage.GetCurrentKey().KeyID
-	signed, err := newtoken.SignedString(keyStorage.GetCurrentKey().PrivateKey)
+	}
+
+	tokenstorage := tokenstoragehelper.GetSigningTokenKeyStorage()
+
+	signed, err := tokenstorage.Sign(claims)
 	if err != nil {
 		return "", err
 	}
@@ -180,28 +117,5 @@ func ExtractGroups(user *identitymodels.User) ([]string, error) {
 
 // GetJwks returns the JSON Web Key Set (JWKS) containing the public keys
 func GetJwks() (jwk.Set, error) {
-
-	set := jwk.NewSet()
-	for _, data := range keyStorage.Keys {
-		pubKey := data.PrivateKey.Public().(*rsa.PublicKey)
-		jwkKey, err := jwk.FromRaw(pubKey)
-		if err != nil {
-			return nil, err
-		}
-		if err := jwkKey.Set(jwk.KeyIDKey, data.KeyID); err != nil {
-			return nil, err
-		}
-		if err := jwkKey.Set(jwk.AlgorithmKey, data.AlgorithmKey); err != nil {
-			return nil, err
-		}
-		if err := jwkKey.Set(jwk.KeyUsageKey, "sig"); err != nil {
-			return nil, err
-		}
-
-		if err := set.AddKey(jwkKey); err != nil {
-			return nil, err
-		}
-	}
-
-	return set, nil
+	return tokenstoragehelper.GetTokenKeyStorage().GetJwks()
 }
