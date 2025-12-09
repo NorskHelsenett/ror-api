cda0bdc095db4bdc45fb21e45b5aa5a9cfcd5140
Håvard Elnan <havard.elnan@nhn.no>
2025-12-09 16:42:20 +0100

    refactor: replace resty client with native http client for GitLab requests

--- internal/clients/helsegitlab/helsegitlab.go
+++ internal/clients/helsegitlab/helsegitlab.go
@@ -4,6 +4,8 @@ package helsegitlab
 import (
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"net/url"
 
 	"github.com/NorskHelsenett/ror/pkg/config/rorconfig"
@@ -13,15 +15,13 @@ import (
 	"github.com/NorskHelsenett/ror/pkg/rlog"
 
 	"github.com/NorskHelsenett/ror/pkg/helpers/stringhelper"
-
-	"github.com/go-resty/resty/v2"
 )
 
-var client *resty.Client
-var request *resty.Request
+var httpClient *http.Client
+var gitlabToken string
 
 func GetFileContent(projectId int, filePath string, branch string, vaultClient *vaultclient.VaultClient) ([]byte, error) {
-	gitlabRequest, err := getGitlabRequest(vaultClient)
+	client, token, err := getGitlabClient(vaultClient)
 	if err != nil {
 		rlog.Error("could not get gitlab client", err)
 		return nil, errors.New("could not get gitlab client")
@@ -31,51 +31,69 @@ func GetFileContent(projectId int, filePath string, branch string, vaultClient *
 	urlencodeBranch := url.QueryEscape(branch)
 
 	repourl := fmt.Sprintf("%s%d/repository/files/%s/raw?ref=%s", rorconfig.GetString("HELSEGITLAB_BASE_URL"), projectId, urlencodeFilePath, urlencodeBranch)
-	response, err := gitlabRequest.Get(repourl)
+	request, err := http.NewRequest(http.MethodGet, repourl, nil)
+	if err != nil {
+		rlog.Error("could not create helsegitlab request", err)
+		return nil, errors.New("could not create helsegitlab request")
+	}
+
+	request.Header.Set("PRIVATE-TOKEN", token)
+
+	response, err := client.Do(request)
 	if err != nil {
 		return nil, errors.New("could not get a response from helsegitlab client")
 	}
+	defer response.Body.Close()
 
-	statusCode := response.StatusCode()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		rlog.Error("could not read helsegitlab response body", err)
+		return nil, errors.New("could not read response body from helsegitlab client")
+	}
+
+	statusCode := response.StatusCode
 	if statusCode > 299 {
-		messages, err := stringhelper.JsonToMap(response.String())
+		messages, err := stringhelper.JsonToMap(string(body))
 		if err != nil {
-			return nil, fmt.Errorf("could not get configuration from ror-api, status code: %d, error: %s", statusCode, response.String())
+			return nil, fmt.Errorf("could not get configuration from ror-api, status code: %d, error: %s", statusCode, string(body))
 		}
 
 		return nil, fmt.Errorf("could not get configuration from ror-api, status code: %d, error: %s", statusCode, messages)
 	}
 
-	return response.Body(), nil
+	return body, nil
 }
 
-func getGitlabRequest(vaultClient *vaultclient.VaultClient) (*resty.Request, error) {
-	if request != nil {
-		return request, nil
+func getGitlabClient(vaultClient *vaultclient.VaultClient) (*http.Client, string, error) {
+	if httpClient != nil && gitlabToken != "" {
+		return httpClient, gitlabToken, nil
 	}
 
 	secretPath := "secret/data/v1.0/ror/config/common" // #nosec G101 Jest the path to the token file in the secrets engine
 	vaultData, err := vaultClient.GetSecret(secretPath)
 	if err != nil {
-		return nil, errors.New("could not extract gitlab access token from vault")
+		return nil, "", errors.New("could not extract gitlab access token from vault")
 	}
 
 	commonConfig, ok := vaultData["data"].(map[string]interface{})
 	if !ok {
 		rlog.Error("", fmt.Errorf("data type assertion failed: %T %#v", vaultData["data"], vaultData["data"]))
-		return nil, errors.New("could not extract gitlab access token from vault-data")
+		return nil, "", errors.New("could not extract gitlab access token from vault-data")
 	}
 
-	token := commonConfig["helsegitlabToken"].(string)
+	tokenValue, ok := commonConfig["helsegitlabToken"].(string)
+	if !ok {
+		rlog.Error("", fmt.Errorf("helsegitlabToken type assertion failed: %T %#v", commonConfig["helsegitlabToken"], commonConfig["helsegitlabToken"]))
+		return nil, "", errors.New("could not extract gitlab access token from vault-data")
+	}
 
-	if len(token) < 1 {
+	if len(tokenValue) < 1 {
 		rlog.Error("could not get gitlab access token from vault", fmt.Errorf("token is empty"))
-		return nil, errors.New("could not get gitlab access token from vault")
+		return nil, "", errors.New("could not get gitlab access token from vault")
 	}
 
-	client = resty.New()
-	req := client.R()
-	req.SetHeader("PRIVATE-TOKEN", token)
+	httpClient = &http.Client{}
+	gitlabToken = tokenValue
 
-	return req, nil
+	return httpClient, gitlabToken, nil
 }
