818472d116b95626402f525670a36a50bd18cde6
Håvard Elnan <havard.elnan@nhn.no>
2026-03-13 08:54:44 +0100

    feat: enhance tracing and error handling in resource service methods

--- internal/apiservices/resourcesv2service/resourcesv2service.go
+++ internal/apiservices/resourcesv2service/resourcesv2service.go
@@ -13,11 +13,16 @@ import (
 
 	"github.com/NorskHelsenett/ror/pkg/apicontracts/apiresourcecontracts"
 	"github.com/NorskHelsenett/ror/pkg/clients/mongodb"
+	"github.com/NorskHelsenett/ror/pkg/config/rorconfig"
 	"github.com/NorskHelsenett/ror/pkg/messagebuscontracts"
 	"github.com/NorskHelsenett/ror/pkg/models/aclmodels/rorresourceowner"
 	"github.com/NorskHelsenett/ror/pkg/rlog"
 	"github.com/NorskHelsenett/ror/pkg/rorresources"
 	"github.com/NorskHelsenett/ror/pkg/rorresources/rortypes"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
 )
 
 var (
@@ -27,6 +32,13 @@ var (
 )
 
 func HandleResourceUpdate(ctx context.Context, resource *rorresources.Resource) rorresources.ResourceUpdateResults {
+	ctx, span := otel.GetTracerProvider().Tracer(rorconfig.GetString(rorconfig.TRACER_ID)).Start(ctx, "v2.resourcesv2service.HandleResourceUpdate")
+	defer span.End()
+	span.SetAttributes(
+		attribute.String("resource.uid", resource.GetUID()),
+		attribute.String("resource.action", string(resource.GetRorMeta().Action)),
+	)
+
 	switch resource.GetRorMeta().Action {
 	case rortypes.K8sActionAdd:
 		return NewOrUpdateResource(ctx, resource)
@@ -36,6 +48,8 @@ func HandleResourceUpdate(ctx context.Context, resource *rorresources.Resource) 
 		err := DeleteResource(ctx, resource)
 		if err != nil {
 			rlog.Error("Could not delete resource", err)
+			span.RecordError(err)
+			span.SetStatus(codes.Error, "could not delete resource")
 			return rorresources.ResourceUpdateResults{
 				Results: map[string]rorresources.ResourceUpdateResult{
 					resource.GetUID(): {
@@ -54,6 +68,7 @@ func HandleResourceUpdate(ctx context.Context, resource *rorresources.Resource) 
 			},
 		}
 	default:
+		span.SetStatus(codes.Error, "unknown action")
 		return rorresources.ResourceUpdateResults{
 			Results: map[string]rorresources.ResourceUpdateResult{
 				resource.GetUID(): {
@@ -66,6 +81,14 @@ func HandleResourceUpdate(ctx context.Context, resource *rorresources.Resource) 
 }
 
 func NewOrUpdateResource(ctx context.Context, resource *rorresources.Resource) rorresources.ResourceUpdateResults {
+	ctx, span := otel.GetTracerProvider().Tracer(rorconfig.GetString(rorconfig.TRACER_ID)).Start(ctx, "v2.resourcesv2service.NewOrUpdateResource")
+	defer span.End()
+	span.SetAttributes(
+		attribute.String("resource.uid", resource.GetUID()),
+		attribute.String("resource.apiversion", resource.GetAPIVersion()),
+		attribute.String("resource.kind", resource.GetKind()),
+	)
+
 	ownerref := resource.GetRorMeta().Ownerref
 
 	// Access check
@@ -74,6 +97,7 @@ func NewOrUpdateResource(ctx context.Context, resource *rorresources.Resource) r
 	// Access: create
 	accessObject := aclservice.CheckAccessByRorOwnerref(ctx, ownerref)
 	if !accessObject.Create {
+		span.SetStatus(codes.Error, "access denied")
 		return rorresources.ResourceUpdateResults{
 			Results: map[string]rorresources.ResourceUpdateResult{
 				resource.GetUID(): {
@@ -86,6 +110,8 @@ func NewOrUpdateResource(ctx context.Context, resource *rorresources.Resource) r
 
 	err := resource.ApplyInputFilter()
 	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "could not apply input filter")
 		return rorresources.ResourceUpdateResults{
 			Results: map[string]rorresources.ResourceUpdateResult{
 				resource.GetUID(): {
@@ -105,6 +131,8 @@ func NewOrUpdateResource(ctx context.Context, resource *rorresources.Resource) r
 	err = databaseHelpers.Set(mongoCtx, resource)
 	if err != nil {
 		rlog.Errorc(ctx, "Failed to set resource", err)
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "failed to set resource")
 		return rorresources.ResourceUpdateResults{
 			Results: map[string]rorresources.ResourceUpdateResult{
 				resource.GetUID(): {
@@ -117,9 +145,11 @@ func NewOrUpdateResource(ctx context.Context, resource *rorresources.Resource) r
 
 	if err := sendToMessageBus(ctx, resource, resource.RorMeta.Action); err != nil {
 		rlog.Errorc(ctx, "Failed to send message to bus", err)
+		span.RecordError(err)
 	}
 
 	//rlog.Debug("Resource created", rlog.Any("resource", resource.GetAPIVersion()), rlog.Any("kind", resource.GetKind()), rlog.Any("name", resource.GetName()))
+	span.SetStatus(codes.Ok, "")
 	return rorresources.ResourceUpdateResults{
 		Results: map[string]rorresources.ResourceUpdateResult{
 			resource.GetUID(): {
@@ -131,6 +161,10 @@ func NewOrUpdateResource(ctx context.Context, resource *rorresources.Resource) r
 }
 
 func GetResourceByUID(ctx context.Context, uid string) *rorresources.ResourceSet {
+	ctx, span := otel.GetTracerProvider().Tracer(rorconfig.GetString(rorconfig.TRACER_ID)).Start(ctx, "v2.resourcesv2service.GetResourceByUID")
+	defer span.End()
+	span.SetAttributes(attribute.String("resource.uid", uid))
+
 	var returnrs *rorresources.ResourceSet
 	//cache := GetResourceCache()
 	//resource := cache.Get(ctx, uid)
@@ -147,6 +181,8 @@ func GetResourceByUID(ctx context.Context, uid string) *rorresources.ResourceSet
 	querystart := time.Now()
 	returnrs, err = databaseHelpers.Get(mongoCtx, query)
 	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "could not get resource by uid")
 		rlog.Error("Could not get resource by uid", err, rlog.String("uid", uid), rlog.Any("error", err))
 		return nil
 	}
@@ -167,14 +203,21 @@ func GetResourceByUID(ctx context.Context, uid string) *rorresources.ResourceSet
 	for _, resource := range returnrs.Resources {
 		accessModel := aclservice.CheckAccessByRorOwnerref(ctx, resource.GetRorMeta().Ownerref)
 		if !accessModel.Read {
+			span.SetStatus(codes.Error, "access denied")
 			return nil
 		}
 	}
 
+	span.SetStatus(codes.Ok, "")
+	span.SetAttributes(attribute.Int("resources.count", len(returnrs.Resources)))
 	return returnrs
 }
 
 func DeleteResource(ctx context.Context, resource *rorresources.Resource) error {
+	ctx, span := otel.GetTracerProvider().Tracer(rorconfig.GetString(rorconfig.TRACER_ID)).Start(ctx, "v2.resourcesv2service.DeleteResource")
+	defer span.End()
+	span.SetAttributes(attribute.String("resource.uid", resource.GetUID()))
+
 	// Access check
 	// Scope: input.Owner.Scope
 	// Subject: input.Owner.Subject
@@ -183,6 +226,8 @@ func DeleteResource(ctx context.Context, resource *rorresources.Resource) error 
 	accessModel := aclservice.CheckAccessByRorOwnerref(ctx, resource.GetRorMeta().Ownerref)
 	if !accessModel.Update {
 		err := fmt.Errorf("403: No access to uid %s", resource.GetUID())
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "access denied")
 		return err
 	}
 
@@ -191,12 +236,22 @@ func DeleteResource(ctx context.Context, resource *rorresources.Resource) error 
 	databaseHelpers := NewResourceMongoDB(mongodb.GetMongodbConnection())
 	err := sendToMessageBus(ctx, resource, rortypes.K8sActionDelete)
 	if err != nil {
+		span.RecordError(err)
 		rlog.Errorc(ctx, "unable to send delete action on rabbit queue", err)
 	}
-	return databaseHelpers.Del(ctx, resource)
+	delErr := databaseHelpers.Del(ctx, resource)
+	if delErr != nil {
+		span.RecordError(delErr)
+		span.SetStatus(codes.Error, "failed to delete resource")
+		return delErr
+	}
+	span.SetStatus(codes.Ok, "")
+	return nil
 }
 
 func GetResourceByQuery(ctx context.Context, query *rorresources.ResourceQuery) (*rorresources.ResourceSet, error) {
+	ctx, span := otel.GetTracerProvider().Tracer(rorconfig.GetString(rorconfig.TRACER_ID)).Start(ctx, "v2.resourcesv2service.GetResourceByQuery")
+	defer span.End()
 
 	databaseHelpers := NewResourceMongoDB(mongodb.GetMongodbConnection())
 	mongoCtx, cancel := context.WithTimeout(ctx, getTimeout)
@@ -204,6 +259,8 @@ func GetResourceByQuery(ctx context.Context, query *rorresources.ResourceQuery) 
 	queryStart := time.Now()
 	rs, err := databaseHelpers.Get(mongoCtx, query)
 	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "could not get resource by query")
 		rlog.Error("Could not get resource by query", err, rlog.Any("error", err))
 		return nil, fmt.Errorf("could not get resource by query: %w", err)
 	}
@@ -237,6 +294,8 @@ func GetResourceByQuery(ctx context.Context, query *rorresources.ResourceQuery) 
 			checkedOwnerRef[resource.GetRorMeta().Ownerref.String()] = -1
 		}
 	}
+	span.SetStatus(codes.Ok, "")
+	span.SetAttributes(attribute.Int("resources.count", len(returnrs.Resources)))
 	return returnrs, nil
 }
 
@@ -282,6 +341,9 @@ func sendToMessageBus(ctx context.Context, resource *rorresources.Resource, acti
 }
 
 func ResourceGetHashlist(ctx context.Context, owner rorresourceowner.RorResourceOwnerReference) (apiresourcecontracts.HashList, error) {
+	ctx, span := otel.GetTracerProvider().Tracer(rorconfig.GetString(rorconfig.TRACER_ID)).Start(ctx, "v2.resourcesv2service.ResourceGetHashlist")
+	defer span.End()
+
 	query := rorresources.ResourceQuery{
 		OwnerRefs: []rorresourceowner.RorResourceOwnerReference{owner},
 		Limit:     -1,
@@ -289,5 +351,12 @@ func ResourceGetHashlist(ctx context.Context, owner rorresourceowner.RorResource
 	mongoCtx, cancel := context.WithTimeout(ctx, getTimeout)
 	defer cancel()
 	databaseHelpers := NewResourceMongoDB(mongodb.GetMongodbConnection())
-	return databaseHelpers.GetHashlistByQuery(mongoCtx, &query)
+	result, err := databaseHelpers.GetHashlistByQuery(mongoCtx, &query)
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "failed to get hashlist")
+		return result, err
+	}
+	span.SetStatus(codes.Ok, "")
+	return result, nil
 }
