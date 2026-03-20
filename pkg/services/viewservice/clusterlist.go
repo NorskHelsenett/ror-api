d9822525bcf516ce2fa68b32abf551baba78c55d
Håvard Elnan <havard.elnan@nhn.no>
2026-03-20 14:11:05 +0100

    Feature: extend cluster list view with detailed headers and data retrieval

--- pkg/services/viewservice/clusterlist.go
+++ pkg/services/viewservice/clusterlist.go
@@ -2,8 +2,12 @@ package viewservice
 
 import (
 	"context"
+	"time"
 
+	"github.com/NorskHelsenett/ror-api/internal/apiservices/resourcesv2service"
 	"github.com/NorskHelsenett/ror/pkg/apicontracts/v2/apiview"
+	"github.com/NorskHelsenett/ror/pkg/rorresources"
+	"github.com/NorskHelsenett/ror/pkg/rorresources/rortypes"
 )
 
 type clusterlistgenerator struct{}
@@ -33,5 +37,253 @@ func (g *clusterlistgenerator) GetMetadata() apiview.ViewMetadata {
 		Description: "A list view of clusters",
 		Name:        "Cluster List View",
 		Version:     1,
+	}
+}
+
+// BFF4EVAH
+func createClusterListHeaders(_ context.Context, _ ...ViewGeneratorsOption) []apiview.ViewColumn {
+	return []apiview.ViewColumn{
+		{
+			Name:        "clusterUid",
+			Description: "The unique identifier of the cluster",
+			Default:     true,
+			Order:       0,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "clusterId",
+			Description: "The identifier of the cluster",
+			Default:     true,
+			Order:       1,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "clusterName",
+			Description: "The name of the cluster",
+			Default:     true,
+			Order:       2,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "provider",
+			Description: "The provider of the cluster",
+			Default:     true,
+			Order:       3,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "availabilityZone",
+			Description: "The az of the cluster",
+			Default:     true,
+			Order:       4,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "country",
+			Description: "The country where the cluster is located",
+			Default:     true,
+			Order:       4,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "region",
+			Description: "The region where the cluster is located",
+			Default:     true,
+			Order:       5,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "workspace",
+			Description: "Workspace of the cluster",
+			Default:     true,
+			Order:       7,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "environment",
+			Description: "The environment of the cluster",
+			Default:     true,
+			Order:       8,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "resoures",
+			Description: "The resources of the cluster",
+			Default:     true,
+			Order:       9,
+			Type:        apiview.ViewFieldTypeObject,
+		},
+		{
+			Name:        "nodes",
+			Description: "The number of nodes in the cluster",
+			Default:     true,
+			Order:       10,
+			Type:        apiview.ViewFieldTypeNumber,
+		},
+		{
+			Name:        "nodePools",
+			Description: "The number of nodepools in the cluster",
+			Default:     true,
+			Order:       11,
+			Type:        apiview.ViewFieldTypeNumber,
+		},
+		{
+			Name:        "priceMonth",
+			Description: "The price of the cluster per month",
+			Default:     true,
+			Order:       12,
+			Type:        apiview.ViewFieldTypeNumber,
+		},
+		{
+			Name:        "priceYear",
+			Description: "The price of the cluster per year",
+			Default:     true,
+			Order:       13,
+			Type:        apiview.ViewFieldTypeNumber,
+		},
+		{
+			Name:        "ArgocdURL",
+			Description: "The URL to the ArgoCD instance for the cluster",
+			Default:     true,
+			Order:       14,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "grafanaURL",
+			Description: "The URL to the Grafana instance for the cluster",
+			Default:     true,
+			Order:       15,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "rorAgentVersion",
+			Description: "The version of the ROR agent running on the cluster",
+			Default:     true,
+			Order:       16,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "kubernetesVersion",
+			Description: "The version of Kubernetes running on the cluster",
+			Default:     true,
+			Order:       17,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "nhnToolVersion",
+			Description: "The version of the NHN tooling in the cluster",
+			Default:     true,
+			Order:       18,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "serviceID",
+			Description: "The service ID of the cluster",
+			Default:     true,
+			Order:       19,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "tags",
+			Description: "The tags of the cluster",
+			Default:     true,
+			Order:       20,
+			Type:        apiview.ViewFieldTypeObject,
+		},
+		{
+			Name:        "status",
+			Description: "The status of the cluster",
+			Default:     true,
+			Order:       21,
+			Type:        apiview.ViewFieldTypeString,
+		},
+		{
+			Name:        "created",
+			Description: "The date the cluster was created",
+			Default:     true,
+			Order:       22,
+			Type:        apiview.ViewFieldTypeDateTime,
+		},
+		{
+			Name:        "lastSeen",
+			Description: "The last time the cluster was seen",
+			Default:     true,
+			Order:       23,
+			Type:        apiview.ViewFieldTypeDateTime,
+		},
+	}
+}
+
+func createClusterListData(ctx context.Context, _ ...ViewGeneratorsOption) []apiview.ViewRow {
+
+	resourcesService, _ := resourcesv2service.GetResourceByQuery(ctx, &rorresources.ResourceQuery{
+		VersionKind: rortypes.ResourceKubernetesClusterGVK,
+	},
+	)
+	if resourcesService == nil {
+		return []apiview.ViewRow{}
+	}
+	ret := make([]apiview.ViewRow, 0, len(resourcesService.Resources))
+	for _, resource := range resourcesService.Resources {
+		cluster := resource.KubernetesClusterResource
+
+		row := apiview.ViewRow{
+			"clusterUid": {
+				FieldValue: resource.Metadata.UID,
+			},
+			"clusterId": {
+				FieldValue: cluster.Status.AgentStatus.ClusterId,
+			},
+			"clusterName": {
+				FieldValue: cluster.Status.AgentStatus.ClusterName,
+			},
+			"provider": {
+				FieldValue: cluster.Status.AgentStatus.KubernetesProvider.String(),
+			},
+			"availabilityZone": {
+				FieldValue: cluster.Status.AgentStatus.Az,
+			},
+			"country": {
+				FieldValue: cluster.Status.AgentStatus.Country,
+			},
+			"region": {
+				FieldValue: cluster.Status.AgentStatus.Region,
+			},
+			"workspace": {
+				FieldValue: cluster.Status.AgentStatus.Workspace,
+			},
+			"environment": {
+				FieldValue: cluster.Status.AgentStatus.Environment,
+			},
+			"nodes": {
+				FieldValue: 799,
+			},
+			"rorAgentVersion": {
+				FieldValue: cluster.Status.AgentStatus.Versions["RorAgent"],
+			},
+			"lastSeen": {
+				FieldValue: cluster.Status.AgentStatus.LastSeen,
+			},
+			"created": {
+				FieldValue: cluster.Status.AgentStatus.CreatedAt,
+			},
+			"status": {
+				FieldValue: calculateStatus(cluster.Status.AgentStatus.LastSeen),
+			},
+			// Add more fields as needed
+		}
+		ret = append(ret, row)
+	}
+	return ret
+}
+
+func calculateStatus(last time.Time) string {
+	diff := time.Since(last)
+	if diff < 5*time.Minute {
+		return "ok"
+	} else if diff < 15*time.Minute {
+		return "warning"
+	} else {
+		return "error"
 	}
 }
