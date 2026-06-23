1329f419a9350bb9a631fecd16ee27e2e4ed7d21
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-06-23 13:06:02 +0200

    corrected typo

--- cmd/generator/main.go
+++ cmd/generator/main.go
@@ -1,5 +1,5 @@
 // The Generator package provides a way to generate code for collecting,
-// transfering and saving resources in the agent and api.
+// transferring and saving resources in the agent and api.
 // It also provides functions to fetc the resources.
 //
 //	go run build/generator/main.go
