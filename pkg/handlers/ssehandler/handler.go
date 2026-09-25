d36c4e9fb4725ecb0def13ab3e65e81a1aff8041
Anne Beth Andersen <195699472+abla86@users.noreply.github.com>
2026-09-25 22:06:14 +0200

    fix: stop SSE handlers after publish errors

--- pkg/handlers/ssehandler/handler.go
+++ pkg/handlers/ssehandler/handler.go
@@ -107,6 +107,7 @@ func Send() gin.HandlerFunc {
 		if err != nil {
 			rerr := rorginerror.NewRorGinError(http.StatusInternalServerError, "could not send sse broadcast event", err)
 			rerr.GinLogErrorAbort(c)
+			return
 		}
 		c.JSON(http.StatusOK, nil)
 	}
